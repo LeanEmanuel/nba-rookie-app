5288e29d6942aaa2e63a0ffe0044513528bb43a8
Leandro Struni <164394186+LeanEmanuel@users.noreply.github.com>
2025-04-09 23:36:55 +0200

    feat(auth): implement login page with reactive form and Ionic UI

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonNote,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonNote,
+    IonButton,
+    RouterLink]
 })
-export class LoginPage implements OnInit {
 
-  constructor() { }
+export class LoginPage {
 
-  ngOnInit() {
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(private fb: FormBuilder, private router: Router) {
   }
 
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.router.navigateByUrl('/player-list');
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
